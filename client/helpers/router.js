8e907657474605ef9d5456f1c7fca44b576a10bf
parhelium <me@parhelium.pl>
2013-05-28 09:37:48 +0200

    ch7-7  : security added

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -11,8 +11,11 @@ Meteor.Router.filters({
   'requireLogin': function(page) {
     if (Meteor.user())
       return page;
+    else if (Meteor.loggingIn())
+      return 'loading';
     else
       return 'accessDenied';
   }
 });
+
 Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
