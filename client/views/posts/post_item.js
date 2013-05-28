5844d421d69160dea2b1390035253668bd31d661
parhelium <me@parhelium.pl>
2013-05-28 10:44:58 +0200

    improved postItem.helpers.ownPost

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,10 +1,11 @@
 Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId && this.userId == Meteor.userId() ;
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  },
-  ownPost: function() {
-    return this.userId == Meteor.userId();
   }
+  
 });
